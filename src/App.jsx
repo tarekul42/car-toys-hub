39d92f9e00b08c8b7b6b3a0986e02dbfc4b3ca78
tarekul42 <tarekulrifat205@gmail.com>
2023-11-24 12:07:48 -0800

    primary setup for car toys hub is done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Button from '@mui/material/Button';
+
+function App() {
+
+  return (
+    <div className='text-center'>
+    <Button variant="contained">Hello world</Button>
+      <h1>Car Toys</h1>
+    </div>
+  )
+}
+
+export default App
